4cd9f732f81916cc1cdc155d81ab40ef27141331
118k <fluorite118@gmail.com>
2019-05-15 21:53:24 +0900

    add: README.rst

---
+++ yarn-rest-api/README.rst
@@ -0,0 +1,10 @@
+This is Python Library for Hadoop YARN rest api.
+
+This project follows the [Resource Manager Rest](https://hadoop.apache.org/docs/current/hadoop-yarn/hadoop-yarn-site/ResourceManagerRest.html) document.
+
+Usage
+=====
+
+Install it::
+
+    pip install hadoop-yarn-rest-api
--- yarn-rest-api/setup.py
+++ yarn-rest-api/setup.py
@@ -1,14 +1,20 @@
 from setuptools import setup, find_packages
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+        
 setup(
     name             = 'hadoop-yarn-rest-api',
     version          = '0.1',
     description      = 'Python wrapper for Hadoop YARN REST API',
-    long_description = open('README.md').read(),
+    #long_description = open('README.rst').read(),
+    long_description=long_description,
+    long_description_content_type='text/markdown; charset=UTF-8',
     author           = 'hs_seo',
     author_email     = 'fluorite118@gmail.com',
     url              = 'https://github.com/developer-sdk/yarn-rest-api',
     download_url     = 'https://github.com/developer-sdk/yarn-rest-api/archive/master.zip',
-    packages         = find_packages(),
-    keywords         = ['hadoop', 'yarn', 'rest', 'api']
+    packages         = find_packages(exclude = ['docs', 'tests*']),
+    keywords         = ['hadoop', 'yarn', 'rest', 'api'],
+    zip_safe         = False
 )
