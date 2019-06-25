95487f1638a48223422b34f9c567f8ec63f035a6
118k <fluorite118@gmail.com>
2019-06-25 23:43:36 +0900

    modify: add ssl certification

--- yarn-rest-api/hadoop/httputil3.py
+++ yarn-rest-api/hadoop/httputil3.py
@@ -2,6 +2,17 @@
 # -*- coding: utf-8 -*-
 import urllib.request, urllib.parse, urllib.error, urllib.request, urllib.error, urllib.parse
 from urllib.error import HTTPError
+import ssl
+
+try:
+    _create_unverified_https_context = ssl._create_unverified_context
+except AttributeError:
+    # Legacy Python that doesn't verify HTTPS certificates by default
+    pass
+else:
+    # Handle target environment that doesn't support HTTPS verification
+    ssl._create_default_https_context = _create_unverified_https_context
+
 '''
 @author: whitebeard-k
 '''
